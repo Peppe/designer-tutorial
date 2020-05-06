de2362ea6d9ceed450a7542267b8429351e42aee
Jens Jansson <fluffysquirrel@outlook.com>
2020-05-06 09:01:04 +0300

    Designer tutorial after 'Enable filtering'

---
+++ frontend/src/views/contact-form.js
@@ -0,0 +1,51 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-form-layout/src/vaadin-form-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-text-field/src/vaadin-email-field.js';
+import '@vaadin/vaadin-combo-box/src/vaadin-combo-box.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+
+class ContactForm extends PolymerElement {
+
+    static get template() {
+        return html`
+<style include="shared-styles">
+                :host {
+                    display: block;
+                    height: 100%;
+                }
+            </style>
+<vaadin-form-layout style="width: 100%; height: 100%;">
+ <vaadin-text-field label="First name" id="firstName"></vaadin-text-field>
+ <vaadin-text-field label="Last name" id="lastName"></vaadin-text-field>
+ <vaadin-email-field label="Email" id="emailField"></vaadin-email-field>
+ <vaadin-combo-box label="Company" id="companyField"></vaadin-combo-box>
+ <vaadin-combo-box label="Status" id="statusField"></vaadin-combo-box>
+ <vaadin-horizontal-layout theme="spacing">
+  <vaadin-button theme="primary" id="saveButton">
+    Save 
+  </vaadin-button>
+  <vaadin-button theme="error" id="deleteButton">
+    Delete 
+  </vaadin-button>
+  <vaadin-button theme="tertiary" id="closeButton">
+    Close 
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+</vaadin-form-layout>
+`;
+    }
+
+    static get is() {
+        return 'contact-form';
+    }
+
+    static get properties() {
+        return {
+            // Declare your properties here.
+        };
+    }
+}
+
+customElements.define(ContactForm.is, ContactForm);
---
+++ src/main/java/com/vaadin/tutorial/crm/ui/ContactForm.java
@@ -0,0 +1,74 @@
+package com.vaadin.tutorial.crm.ui;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.tutorial.crm.backend.entity.Company;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.CompanyService;
+
+import java.util.List;
+
+/**
+ * A Designer generated component for the contact-form template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("contact-form")
+@JsModule("./src/views/contact-form.js")
+public class ContactForm extends PolymerTemplate<ContactForm.ContactFormModel> {
+
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+    @Id("firstName")
+    private TextField firstName;
+    @Id("lastName")
+    private TextField lastName;
+    @Id("emailField")
+    private EmailField email;
+    @Id("companyField")
+    private ComboBox<Company> company;
+    @Id("statusField")
+    private ComboBox<Contact.Status> status;
+    @Id("saveButton")
+    private Button saveButton;
+    @Id("deleteButton")
+    private Button deleteButton;
+    @Id("closeButton")
+    private Button closeButton;
+    @Id("vaadinTabs")
+    private Tabs vaadinTabs;
+
+    /**
+     * Creates a new ContactForm.
+     */
+    public ContactForm(CompanyService companyService) {
+        new Span(new Text("hello world"));
+        // You can initialise any data required for the connected UI components here.
+        binder.bindInstanceFields(this);
+
+        company.setItems(companyService.findAll());
+        company.setItemLabelGenerator(Company::getName);
+        status.setItems(Contact.Status.values());
+
+    }
+
+    /**
+     * This model binds properties between ContactForm and contact-form
+     */
+    public interface ContactFormModel extends TemplateModel {
+        // Add setters and getters for template properties here.
+    }
+}
